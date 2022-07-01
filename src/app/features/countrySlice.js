fad463b1d60a6db4b3edf7f9367df54d2d50a244
Talha <tkz9696@gmail.com>
2022-07-02 03:12:45 +0500

    Table added

--- src/app/features/countrySlice.js
+++ src/app/features/countrySlice.js
@@ -1,12 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import { finalUrl } from '../../Pages/CountryInfo';
 
 export const fetchCountryInfo = createAsyncThunk(
   'countryInfo/getCountryInfo',
   async (x) => {
     const response = await fetch(x);
-    const countryInfo = response.json();
-    return countryInfo;
+    const countryInfo = await response.json();
+    return countryInfo.list[0].components;
   },
 );
 
